00f7380281e8ce08dcab0deb8c9b7eb0015104cd
Mickey4527 <saran.za097@gmail.com>
2023-12-16 23:50:28 +0700

    Refactor Triangle function and fix decimal rounding bug

--- calc.php
+++ calc.php
@@ -1,7 +1,4 @@
 <?php
-    $a = 10;
-    $b = 20;
-    $c = 30;
 
     // a + b > c , b + c > a , c + a > b -> triangle
     // a = b = c -> equilateral
@@ -14,13 +11,12 @@
 
 
     function Triangle($side_one, $side_two, $side_three){
-        // decimal to 2 digit
-        $side_one  = round($side_one, 2);
-        $side_two  = round($side_two, 2);
-        $side_three  = round($side_three, 2);
-
         // check value is numeric or not
         if(is_numeric($side_one) && is_numeric($side_two) && is_numeric($side_three)){
+            // decimal to 2 digit
+            $side_one  = round($side_one, 2);
+            $side_two  = round($side_two, 2);
+            $side_three  = round($side_three, 2);
             // if value < 100
             if(($side_one && $side_two && $side_three) >= 0 && ($side_one && $side_two && $side_three) <= 100){ //Warning
                 if($side_one + $side_two > $side_three && $side_two + $side_three > $side_one && $side_three + $side_one > $side_two){
@@ -50,5 +46,6 @@
         }
     }
 
+    Triangle($_POST['side_one'], $_POST['side_two'], $_POST['side_three']);
     
 ?>
---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form style="text-align: center;" action="calc.php" method="post">
+        <label for="side_one">Side One</label>
+        <input type="number" name="side_one" id="side_one" placeholder="Enter Side One" min="0" max="100" step="0.01">
+        <br><br>
+        <label for="side_two">Side Two</label>
+        <input type="number" name="side_two" id="side_two" placeholder="Enter Side Two" min="0" max="100" step="0.01">
+        <br><br>
+        <label for="side_three">Side Three</label>
+        <input type="number" name="side_three" id="side_three" placeholder="Enter Side Three" min="0" max="100" step="0.01">
+        <br><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+    
+    <script>
+        let side_one = document.getElementById('side_one');
+        let side_two = document.getElementById('side_two');
+        let side_three = document.getElementById('side_three');
+
+        side_one.addEventListener('input', function(){
+            if(side_one.value > 100){
+                alert('Please enter value between 0 to 100');
+                side_one.value = '';
+            }
+            if(side_one.type != 'number'){
+                alert('นายแอบเปลี่ยน type หรอ');
+                side_one.value = '';
+            }
+        });
+        side_two.addEventListener('input', function(){
+            if(side_two.value > 100){
+                alert('Please enter value between 0 to 100');
+                side_two.value = '';
+            }
+            if(side_two.type != 'number'){
+                alert('นายแอบเปลี่ยน type หรอ');
+                side_one.value = '';
+            }
+        });
+        side_three.addEventListener('input', function(){
+            if(side_three.value > 100){
+                alert('Please enter value between 0 to 100');
+                side_three.value = '';
+            }
+            if(side_three.type != 'number'){
+                alert('นายแอบเปลี่ยน type หรอ');
+                side_one.value = '';
+            }
+        });
+    </script>
+</body>
+</html>
