340ece8ad73bab26d595f4625e66d8a9bd3f427b
Kaurakoch Sriunprasert <kaurakoch.s@gmail.com>
2023-12-17 01:09:51 +0700

    decimal 2 digit prevent

--- calc.php
+++ calc.php
@@ -10,36 +10,45 @@
     // 0.00 <= (a = b = c) => 100.00
 
 
-    function Triangle($side_one, $side_two, $side_three){
+    function Triangle($side_One, $side_Two, $side_Three){
         // check value is numeric or not
-        if(is_numeric($side_one) && is_numeric($side_two) && is_numeric($side_three)){
+        if(is_numeric($side_One) && is_numeric($side_Two) && is_numeric($side_Three)){
+            
             // decimal to 2 digit
-            $side_one  = round($side_one, 2);
-            $side_two  = round($side_two, 2);
-            $side_three  = round($side_three, 2);
-            // if value < 100
+            $side_one  = round($side_One, 2);
+            $side_two  = round($side_Two, 2);
+            $side_three  = round($side_Three, 2);
+            //check value is between 0 to 100
             if(($side_one <= 100 && $side_two <= 100 && $side_three <= 100) && ($side_one >= 0 && $side_two >= 0 && $side_three >= 0)){
-                if($side_one + $side_two > $side_three && $side_two + $side_three > $side_one && $side_three + $side_one > $side_two){
-                    if($side_one == $side_two && $side_two == $side_three){
-                        echo "Equilateral Triangle";
+                //check value is 2 digit or not
+                if(strlen($side_one) > 5 || strlen($side_two) > 5 || strlen($side_three) > 5){
+                    if($side_one + $side_two > $side_three && $side_two + $side_three > $side_one && $side_three + $side_one > $side_two){
+                        if($side_one == $side_two && $side_two == $side_three){
+                            echo "Equilateral Triangle";
+                        }
+                        elseif($side_one == $side_two || $side_two == $side_three || $side_three == $side_one){
+                            echo "Isosceles Triangle";
+                        }
+                        elseif($side_one != $side_two && $side_two != $side_three && $side_three != $side_one){
+                            echo "Scalene Triangle";
+                        }
+                        elseif(pow($side_one, 2) + pow($side_two, 2) == pow($side_three, 2) || pow($side_two, 2) + pow($side_three, 2) == pow($side_one, 2) || pow($side_three, 2) + pow($side_one, 2) == pow($side_two, 2)){
+                            echo "Right Angle Triangle";
+                        }
                     }
-                    elseif($side_one == $side_two || $side_two == $side_three || $side_three == $side_one){
-                        echo "Isosceles Triangle";
+                    else{
+                        echo "Not a Triangle";
                     }
-                    elseif($side_one != $side_two && $side_two != $side_three && $side_three != $side_one){
-                        echo "Scalene Triangle";
-                    }
-                    elseif(pow($side_one, 2) + pow($side_two, 2) == pow($side_three, 2) || pow($side_two, 2) + pow($side_three, 2) == pow($side_one, 2) || pow($side_three, 2) + pow($side_one, 2) == pow($side_two, 2)){
-                        echo "Right Angle Triangle";
-                    }
-                }
-                else{
-                    echo "Not a Triangle";
-                }
+
+                }else{
+                    echo "Please enter value upto 2 digit";
+                } 
+                
             }
             else{
                 echo "Please enter value between 0 to 100";
             }
+
         }
         else{
             echo "Please enter numeric value";
